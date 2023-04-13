7723de3658a6339a77eb9c4eefef44446c47ff62
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-04-13 18:06:57 -0400

    Update ip.tf

--- ip.tf
+++ ip.tf
@@ -1,5 +1,6 @@
 provider "google"{}
-resource "google_compute_global_address""default"{
+project="testing2000"
+resource "google_compute_global_address" "default"{
 name="global-server-ip"
 project="testing2000"
 }
